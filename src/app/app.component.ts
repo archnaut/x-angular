fce3885a98db76211e46134f30e304de03685975
Paul Clements <paulcclements@gmail.com>
2018-04-10 15:04:59 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  greeting = '';
+
+  constructor(private appService: AppService){}
+
+
+  ngOnInit(): void {
+    this.appService.sayHello()
+      .subscribe(result => this.greeting = result);
+  }
 }
